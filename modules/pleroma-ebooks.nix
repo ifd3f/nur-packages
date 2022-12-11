e6e07b0d286053097a848ee4eef347c3fac99a3d
Astrid Yu <astrid@astrid.tech>
2022-12-11 12:59:16 -0800

    Add config generation script

--- modules/pleroma-ebooks.nix
+++ modules/pleroma-ebooks.nix
@@ -3,62 +3,82 @@ with lib;
 let cfg = config.services.pleroma-ebooks;
 in {
   options.services.pleroma-ebooks.bots = let
-    botOpts = { name, ... }: {
-      options = {
-        accessTokenFile = mkOption {
-          description = ''
-            Path to access token file.
+    botOpts = { name, ... }:
+      let botCfg = cfg.${name};
+      in {
+        options = {
+          accessTokenFile = mkOption {
+            description = ''
+              Path to access token file.
 
-            You can generate one here: https://tools.splat.soy/pleroma-access-token
-          '';
-          type = types.str;
-          default = "/var/lib/secrets/pleroma-ebooks/${name}";
-        };
+              You can generate one here: https://tools.splat.soy/pleroma-access-token
+            '';
+            type = types.str;
+            default = "/var/lib/secrets/pleroma-ebooks/${name}";
+          };
 
-        dbPath = mkOption {
-          description = "Path to store the database";
-          type = types.str;
-          default = "/var/lib/pleroma-ebooks/${name}";
-        };
+          dbPath = mkOption {
+            description = "Path to store the database";
+            type = types.str;
+            default = "/var/lib/pleroma-ebooks/${name}";
+          };
 
-        fetchOnCalendar = mkOption {
-          description =
-            "How often to fetch. See systemd.time(7) for more information about the format.";
-          type = types.str;
-          default = "hourly";
-        };
+          site = mkOption {
+            description = "Site to post on";
+            type = types.str;
+          };
 
-        postOnCalendar = mkOption {
-          description =
-            "How often to post. See systemd.time(7) for more information about the format.";
-          type = types.str;
-          default = "hourly";
-        };
+          fetchOnCalendar = mkOption {
+            description =
+              "How often to fetch. See systemd.time(7) for more information about the format.";
+            type = types.str;
+            default = "hourly";
+          };
 
-        extraConfig = mkOption {
-          description = ''
-            Additional options to add to the config.
+          postOnCalendar = mkOption {
+            description =
+              "How often to post. See systemd.time(7) for more information about the format.";
+            type = types.str;
+            default = "hourly";
+          };
 
-            See the default config for more details: https://github.com/ioistired/pleroma-ebooks/blob/master/config.defaults.json
-          '';
-          type = types.attrs;
-          default = {
-            cw = null;
-            learn_from_cw = false;
-            ignored_cws = [ ];
-            mention_handling = 1;
-            max_thread_length = 15;
-            strip_paired_punctuation = false;
-            limit_length = false;
-            length_lower_limit = 5;
-            length_upper_limit = 50;
-            overlap_ratio_enabled = false;
-            overlap_ratio = 0.7;
-            generation_mode = "markov";
+          extraConfig = mkOption {
+            description = ''
+              Additional options to add to the config.
+
+              See the default config for more details: https://github.com/ioistired/pleroma-ebooks/blob/master/config.defaults.json
+            '';
+            type = types.attrs;
+            default = {
+              cw = null;
+              learn_from_cw = false;
+              ignored_cws = [ ];
+              mention_handling = 1;
+              max_thread_length = 15;
+              strip_paired_punctuation = false;
+              limit_length = false;
+              length_lower_limit = 5;
+              length_upper_limit = 50;
+              overlap_ratio_enabled = false;
+              overlap_ratio = 0.7;
+              generation_mode = "markov";
+            };
+          };
+
+          config = mkOption {
+            description = ''
+              The config to write in, minus the access_token attribute. You should use `extraConfig` and the other attributes instead.
+
+              See the default config for more details: https://github.com/ioistired/pleroma-ebooks/blob/master/config.defaults.json
+            '';
+            type = types.attrs;
+            default = {
+              site = botCfg.site;
+              db_path = botCfg.dbPath;
+            } // botCfg.extraConfig;
           };
         };
       };
-    };
 
   in mkOption {
     type = with types; attrsOf (submodule botOpts);
@@ -69,13 +89,36 @@ in {
   };
 
   config = mkMerge (mapAttrsToList (name: botCfg:
-    let unitName = "pleroma-ebooks-${name}";
+    let
+      unitName = "pleroma-ebooks-${name}";
+      baseConfigJSON = pkgs.writeText "pleroma-ebooks-${name}-config.json"
+        (builtins.toJSON botCfg.config);
+      generate-pleroma-ebooks-config = pkgs.writeShellApplication {
+        name = "generate-pleroma-ebooks-config";
+        runtimeInputs = with pkgs; [ coreutils jq pleroma-ebooks ];
+        text = ''
+          config="$(mktemp)"
+          chmod 600 "$config"
+
+          access_token="$(cat "$ACCESS_TOKEN_FILE")"
+          jq --arg token "$access_token" '. + {access_token: $token}' < "$BASE_CONFIG_FILE" > "$config"
+
+          echo "$config"
+        '';
+      };
     in {
       systemd.services."${unitName}-post" = {
         wantedBy = [ "network-online.target" ];
-        path = with pkgs; [ pleroma-ebooks ];
+        path = with pkgs; [ generate-pleroma-ebooks-config pleroma-ebooks ];
+        environment = {
+          ACCESS_TOKEN_FILE = botCfg.accessTokenFile;
+          BASE_CONFIG_FILE = baseConfigJSON;
+        };
+        unitConfig.ConditionPathExists = botCfg.accessTokenFile;
         script = ''
-          gen.py -c ${configJSON}
+          config="$(generate-pleroma-ebooks-config)"
+          gen.py -c "$config"
+          rm "$config"
         '';
       };
       systemd.timers."${unitName}-post" = {
@@ -85,9 +128,16 @@ in {
 
       systemd.services."${unitName}-fetch" = {
         wantedBy = [ "network-online.target" ];
-        path = with pkgs; [ pleroma-ebooks ];
+        path = with pkgs; [ generate-pleroma-ebooks-config pleroma-ebooks ];
+        environment = {
+          ACCESS_TOKEN_FILE = botCfg.accessTokenFile;
+          BASE_CONFIG_FILE = baseConfigJSON;
+        };
+        unitConfig.ConditionPathExists = botCfg.accessTokenFile;
         script = ''
-          fetch_posts.py -c ${configJSON}
+          config="$(generate-pleroma-ebooks-config)"
+          fetch-posts.py -c "$config"
+          rm "$config"
         '';
       };
       systemd.timers."${unitName}-fetch" = {
